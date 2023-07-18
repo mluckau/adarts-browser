7d37324967dcaf0aacc37ec1718b22dace9e76ca
Michael <mluckau@gmail.com>
2023-07-18 22:35:13 +0200

    Signals versuch

--- darts-browser.py
+++ darts-browser.py
@@ -34,6 +34,11 @@ class MyWebBrowser(QMainWindow):
         self.browser1 = QWebEngineView()
         self.browser2 = QWebEngineView()
 
+        self.browser1.loadStarted.connect(self.onLoadFinished)
+
+        self.browser1.urlChanged.connect(self.onLoadFinished)
+        self.browser2.loadFinished.connect(self.onLoadFinished)
+
         # self.storage_location = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.AppDataLocation)
 
         self.profile1 = QWebEngineProfile("storage-1", self.browser1)
@@ -42,18 +47,23 @@ class MyWebBrowser(QMainWindow):
         self.webpage1 = QWebEnginePage(self.profile1, self.browser1)
         self.webpage2 = QWebEnginePage(self.profile2, self.browser2)
 
+        self.webpage1.loadFinished.connect(self.onLoadFinished)
+
         self.browser1.setPage(self.webpage1)
         self.browser2.setPage(self.webpage2)
 
         self.layout.addWidget(self.browser1)
         self.layout.addWidget(self.browser2)
 
-        self.browser1.load(QUrl(url1))
+        self.browser1.setUrl(QUrl(url1))
         self.browser2.load(QUrl(url2))
 
         self.window.setLayout(self.layout)
         # self.window.showMaximized()
         self.window.showFullScreen()
+
+    def onLoadFinished(self):
+        print("Hallo")
 
 
 app = QApplication(sys.argv)

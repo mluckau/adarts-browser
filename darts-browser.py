ed509bc91cdc82152fbc00425499a3b31f7e9471
Michael <mluckau@gmail.com>
2023-07-20 19:17:18 +0200

    style angepasst

--- darts-browser.py
+++ darts-browser.py
@@ -1,17 +1,61 @@
 import sys
 import os
 import configparser
-from PyQt6 import QtCore
-from PyQt6.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QHBoxLayout, QWidget
-from PyQt6.QtCore import QUrl
-from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6.QtWebEngineCore import QWebEngineProfile, QWebEnginePage
+
+# from PyQt6 import QtCore
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QApplication,
+    QVBoxLayout,
+    QWidget,
+)
+from PySide6.QtCore import QUrl, QFile, Qt
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWebEngineCore import (
+    QWebEngineProfile,
+    QWebEnginePage,
+    QWebEngineScript,
+    QWebEngineSettings,
+)
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-url1 = config["url"]["url1"]
-url2 = config["url"]["url2"]
+url1 = config.get("url", "url1")
+url2 = config.get("url", "url2")
+
+css_style = config.getboolean("style", "activate")
+
+
+def injectCSS(view, path, name):
+    path = QFile(path)
+    print(path.fileName())
+    if not path.open(QFile.ReadOnly | QFile.Text):
+        print("Huhu")
+        return
+    css = path.readAll().data().decode("utf-8")
+    # print(css)
+    SCRIPT = """
+    (function() {
+    css = document.createElement('style');
+    css.type = 'text/css';
+    css.id = "%s";
+    document.head.appendChild(css);
+    css.innerText = `%s`;
+    })()
+    """ % (
+        name,
+        css,
+    )
+
+    script = QWebEngineScript()
+    view.page().runJavaScript(SCRIPT, QWebEngineScript.ApplicationWorld)
+    script.setName(name)
+    script.setSourceCode(SCRIPT)
+    script.setInjectionPoint(QWebEngineScript.DocumentReady)
+    script.setRunsOnSubFrames(True)
+    script.setWorldId(QWebEngineScript.ApplicationWorld)
+    view.page().scripts().insert(script)
 
 
 class MyWebBrowser(QMainWindow):
@@ -19,13 +63,14 @@ class MyWebBrowser(QMainWindow):
         super(MyWebBrowser, self).__init__(*args, **kwargs)
         self.initUi()
         self.loadPages()
-        # self.showWindow()
 
     def initUi(self):
         self.window = QWidget()
         self.window.setWindowTitle("Autodarts Webbrowser")
-        self.window.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
-        # self.window.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.window.setWindowFlag(Qt.WindowType.FramelessWindowHint, True)
+        self.window.setWindowFlag(Qt.WindowStaysOnTopHint, True)
+        self.window.setStyleSheet("background-color: rgb(0, 0, 0);")
+        # self.window.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
 
         # TODO Auswahl des Bildschirms implementieren
         """ self.screens = app.screens()
@@ -33,45 +78,46 @@ class MyWebBrowser(QMainWindow):
         self.window.setScreen(self.screens[0]) """
 
         self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
 
         # Setup Browser 1
         self.browser1 = QWebEngineView(self)
         self.profile1 = QWebEngineProfile("storage-1", self.browser1)
         self.profile1.setPersistentStoragePath(os.getcwd() + ("/Storage1"))
         self.webpage1 = QWebEnginePage(self.profile1, self.browser1)
+        self.webpage1.loadFinished.connect(self._on_Load_Finished)
+        self.webpage1.settings().setAttribute(
+            QWebEngineSettings.WebAttribute.ShowScrollBars, False
+        )
         self.browser1.setPage(self.webpage1)
         self.layout.addWidget(self.browser1)
 
         # Setup Browser 2
-        self.browser2 = QWebEngineView()
+        self.browser2 = QWebEngineView(self)
         self.profile2 = QWebEngineProfile("storage-2", self.browser2)
         self.profile2.setPersistentStoragePath(os.getcwd() + ("/Storage2"))
         self.webpage2 = QWebEnginePage(self.profile2, self.browser2)
+        self.webpage2.loadFinished.connect(self._on_Load_Finished)
         self.browser2.setPage(self.webpage2)
         self.layout.addWidget(self.browser2)
 
-        # set SignalHandler
-        # TODO implement Signals um die geladenen Seiten mit Javascript zu manipulieren
-        self.browser1.loadFinished.connect(self.onLoadFinished)
-        self.browser2.loadFinished.connect(self.onLoadFinished)
-
     def loadPages(self):
-        self.browser1.setUrl(QUrl(url1))
-        self.browser2.setUrl(QUrl(url2))
+        self.browser1.page().setUrl(QUrl(url1))
+        self.browser2.page().setUrl(QUrl(url2))
 
     def showWindow(self):
         self.window.setLayout(self.layout)
         self.window.showFullScreen()
 
-    def onLoadFinished(self, ok):
-        # TODO implemt Code um die Seite zu manipulieren, Titelleiste ausblenden etc.
-        if ok:
-            print("Hallo")
+    def _on_Load_Finished(self, ok):
+        print("onLoadFinished triggered")
+        if ok and css_style:
+            injectCSS(self.browser1, os.getcwd() + "/style.css", "injectedCSS")
 
 
 # FIXME
 # Release of profile requested but WebEnginePage still not deleted. Expect troubles !
-# https://stackoverflow.com/questions/64719361/closing-qwebengineview-warns-release-of-profile-requested-but-webenginepage-sti
+# https://sdestackoverflow.com/questions/64719361/closing-qwebengineview-warns-release-of-profile-requested-but-webenginepage-sti
 
 
 def main():

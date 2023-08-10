2b14a9e759131a1a71fc663d911b9cf439c837f1
Michael <mluckau@gmail.com>
2023-08-10 22:41:04 +0200

    reihenfolge geändert

--- darts-browser.py
+++ darts-browser.py
@@ -16,8 +16,8 @@ from PyQt5.QtWebEngineWidgets import (
     QWebEngineSettings,
 )
 
-config = configparser.ConfigParser()
 working_path, filename = os.path.split(os.path.abspath(__file__))
+config = configparser.ConfigParser()
 config.read(working_path + "/config.ini")
 
 url1 = f'https://autodarts.io/boards/{config.get("boards", "board1_id")}/follow'
